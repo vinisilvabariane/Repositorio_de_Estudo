ac0155bef013da858c4c6fd5252e911d2b46e167
ViniciusBariane <146668696+vinisilvabariane@users.noreply.github.com>
2025-05-13 10:19:03 -0300

    cleanup: Typecasting.py

--- Python/02 - Typecasting.py
+++ Python/02 - Typecasting.py
@@ -1,9 +1,9 @@
-# conversão de tipos, coerção
-# type convertion, typecasting, coercion
-# é o ato de converter um tipo em outro
-#  tipos imutáveis e primitivos:
-# str, int, float, bool
+
 print(int('1'), type(int('1')))
 print(float('1') + 1, type(float('1')))
 print(bool(' '))
 print(str(11) + 'b')
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
